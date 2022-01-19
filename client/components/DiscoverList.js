2b019b69cbb4bc4d2aaba342a28cdd7e4c17782c
edakhmetov <78399160+edakhmetov@users.noreply.github.com>
2022-01-18 19:18:44 -0500

    added styles for discover page

---
+++ client/components/DiscoverList.js
@@ -0,0 +1,45 @@
+import apiService from '../lib/api/apiService';
+import Link from 'next/link';
+import styles from '../styles/Discover.module.css';
+
+const DiscoverList = ({ user, users }) => {
+
+  const isFollowed = (id) => {
+    return user.followings.filter(u => u.followerId === id).length > 0 ? true : false;
+  }
+
+  const follow = async (id) => {
+    const data = await apiService.followUser(id);
+    // setFollowing(true);
+    // console.log(data);
+  }
+
+  // console.log(users);
+
+  const unfollow = async (id) => {
+    const data = await apiService.unfollowUser(id);
+    // setFollowing(false);
+    // console.log(data);
+  }
+
+  return (
+    users.map(u => (
+      <Link key={u.id} href={`/user/${u.id}`}>
+        <div className={styles.userContainer}>
+          <div>
+            <p className={styles.name}>{u.firstName}</p>
+            <p className={styles.name}>{u.lastName}</p>
+          </div>
+          <p className={styles.language}>native language <p>{u.nativeLanguages}</p></p>
+          <hr className={styles.lineBreak} />
+          <p className={styles.language}>learning language <p>{u.learningLanguages}</p></p>
+          {/* {isFollowed(u.id)
+            ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
+            : <button onClick={() => follow(u.id)}>Follow</button>} */}
+        </div>
+      </Link>
+    ))
+  )
+}
+
+export default DiscoverList
--- client/pages/discover.js
+++ client/pages/discover.js
@@ -1,55 +1,30 @@
 import { useState, useEffect, useContext } from 'react';
-import apiService from '../lib/api/apiService';
 import { AuthContext } from '../lib/context/authContext';
+import apiService from '../lib/api/apiService';
+import DiscoverList from '../components/DiscoverList';
+import styles from '../styles/Discover.module.css';
 
 
 const discover = () => {
 
   const { user } = useContext(AuthContext);
   const [nativeSpeakers, setNativeSpeakers] = useState([]);
-  const [learningSpeakers, setLearningSpeakers] = useState([]);
-  const [following, setFollowing] = useState(false);
-
+  // const [learningSpeakers, setLearningSpeakers] = useState([]);
 
   useEffect(() => {
     (async () => {
       const native = await apiService.getNativeSpeakers();
-      const learning = await apiService.getLearningSpeakers();
-      // console.log(data);
+      // const learning = await apiService.getLearningSpeakers();
+      // console.log(native);
       setNativeSpeakers(native);
-      setLearningSpeakers(learning);
+      // setLearningSpeakers(learning);
     })()
-  }, [])
-
-  // console.log(user);
-
-  const isFollowed = (id) => {
-    return user.followings.filter(u => u.followerId === id).length > 0 ? true : false;
-  }
-
-  const follow = async (id) => {
-    const data = await apiService.followUser(id);
-    // setFollowing(true);
-    // console.log(data);
-  }
-
-  const unfollow = async (id) => {
-    const data = await apiService.unfollowUser(id);
-    // setFollowing(false);
-    // console.log(data);
-  }
+  }, []);
 
   return (
-    <div>
-      {user && <h1>People that know {user.learningLanguages}</h1>}
-      {user && nativeSpeakers.map(u => (
-        <div key={u.id}>
-          <h1>{u.firstName} {u.lastName}</h1>
-          {isFollowed(u.id)
-            ? <button onClick={() => unfollow(u.id)}>Unfollow</button>
-            : <button onClick={() => follow(u.id)}>Follow</button> }
-        </div>
-      ))}
+    <div className={styles.listContainer}>
+      {user && <h1 className={styles.header}>People that know {user.learningLanguages}</h1>}
+      {user && <DiscoverList user={user} users={nativeSpeakers} />}
       {/* {user && <h1>People that know {user.nativeLanguages}</h1>}
       {user && learningSpeakers.map(u => (
         <div key={u.id}>
