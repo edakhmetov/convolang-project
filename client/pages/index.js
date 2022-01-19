b1aec91506c3c79f78b9c720aecf50137d28124c
edakhmetov <78399160+edakhmetov@users.noreply.github.com>
2022-01-18 21:19:06 -0500

    added styles to the home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,14 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { AuthContext } from '../lib/context/authContext'
-import styles from '../styles/Home.module.css'
 import PostList from '../components/PostList';
+import styles from '../styles/Home.module.css'
 
 
 const Home = () => {
 
-  const { user, isLoggedIn } = useContext(AuthContext);
-
-  // useEffect(() => {
-  //   console.log('from the index page', user);
-  // }, [user]);
+  const { user } = useContext(AuthContext);
 
   return (
     <div className={styles.container}>
@@ -20,19 +16,24 @@ const Home = () => {
         <title>Convolang</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {!user && <main>
-        <h1>
-          Welcome to Convolang
-        </h1>
-        <h2>
-          An app where you can learn languages through conversations with native speakers, share your thoughts and knowledge, and make new friends!
-        </h2>
-        <button>
-          <Link href='/register'>
-            Sign Up
-          </Link>
-        </button>
-      </main>}
+      {!user &&
+        <main className={styles.container}>
+          <h1 className={styles.title}>
+            Welcome to Convolang
+          </h1>
+          <h2 className={styles.description}>
+            An app where you can learn languages through conversations with native speakers, share your thoughts and knowledge, and make new friends!
+          </h2>
+          <div className={styles.buttons}>
+            <Link href='/register'>
+              <button className={styles.button}>
+                Sign Up
+              </button>
+            </Link>
+            <p className={styles.login}>Already have account? <Link href='/login'><div>Login</div></Link></p>
+          </div>
+        </main>
+      }
       {user && <div>
         <PostList user={user} />
       </div>}
